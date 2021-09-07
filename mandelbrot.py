cb105da8b4a852b22e3734882e51c8b371798eee
imagine <88944530+imagine1337@users.noreply.github.com>
2021-09-07 15:47:38 -0500

    Create mandelbrot.py

---
+++ mandelbrot.py
@@ -0,0 +1,20 @@
+from PIL import Image, ImageDraw
+w = 600
+h = 400
+im = Image.new('RGB', (w, h), (0, 0, 0))
+draw = ImageDraw.Draw(im)
+def mandelbrot(c):
+    z = 0
+    n = 0
+    while abs(z) <= 2 and n < 100:
+        z = z*z + c
+        n += 1
+    return n
+for x in range(w):
+    for y in range(h):
+        c = complex(-2 + (x / w) * 3,
+                    -1 + (y / h) * 2)
+        m = mandelbrot(c)
+        c = 255 - int(m * 255 / 100)
+        draw.point([x, y], (c, c, c))
+im.save('mandelbrot.png')
